84d671b4ea4d324951604756eae7f705d4655c22
Anthony Boscardin <yayap13@gmail.com>
2016-07-12 20:08:21 +0200

    first commit

---
+++ client/main.js
@@ -0,0 +1,36 @@
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './main.html';
+
+Template.hello.onCreated(function(){
+});
+
+Template.hello.onRendered(function(){
+	var target = new Date('2016-07-16T16:00:00.000Z');
+	function tick() {
+		var now = new Date();
+		var diff = Math.floor((target.getTime() - now.getTime())/1000);
+		
+		var seconds = diff % 60;
+		var minutes = Math.floor(diff%3600 / 60);
+		var hours = Math.floor(diff%86400 / 3600);
+		var days = Math.floor(diff / (60 * 60 * 24));
+		
+		$('.counter .d').html(days<10?"0"+days:days);
+		$('.counter .h').html(hours<10?"0"+hours:hours);
+		$('.counter .m').html(minutes<10?"0"+minutes:minutes);
+		$('.counter .s').html(seconds<10?"0"+seconds:seconds);
+	}
+
+	$(document).ready(function() {
+		window.setInterval(tick, 1000);
+		tick();
+	});
+});
+
+Template.hello.helpers({
+});
+
+Template.hello.events({
+});
